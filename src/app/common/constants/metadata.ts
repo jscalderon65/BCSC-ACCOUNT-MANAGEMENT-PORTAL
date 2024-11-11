9da1386933ed3af793d3e4b3156fef0d896bb039
jcalderon23 <jcalderon@ccxc.us>
2024-11-11 01:44:25 -0500

    [ADD](metadata): configuración de la metadata

---
+++ src/app/common/constants/metadata.ts
@@ -0,0 +1,29 @@
+export const METADATA_TITLE_TEMPLATE = "%s | CajaPlus";
+
+export const METADATA_DEFAULT_TITLE =
+  "Caja Plus - Tu nueva cuenta de ahorro digital - Banco Caja Social | Su Banco Amigo";
+
+export const METADATA_DESCRIPTION =
+  "Descubre Caja Plus, la nueva cuenta de ahorro digital del Banco Caja Social con rendimiento diario de hasta 12% EA. Transfiere y ahorra de forma segura.";
+
+export const METADATA_KEYWORDS = [
+  "banco digital colombia",
+  "cuenta de ahorro digital",
+  "rendimiento diario",
+  "banco caja social",
+  "ahorro con rendimiento",
+];
+
+export const METADATA_ICON_PATH = "/images/favicon.ico";
+
+export const METADATA_OPEN_GRAPH_TITLE = "Caja Plus";
+
+export const METADATA_OPEN_GRAPH_IMAGES = [
+  {
+    url: "/images/preview.png",
+    width: 1200,
+    height: 630,
+  },
+];
+
+export const METADATA_OPEN_GRAPH_LOCALE = "es_CO";
