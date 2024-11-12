69d5002889b6b33fb99130c2743c844cc8ead505
jcalderon23 <jcalderon@ccxc.us>
2024-11-12 02:34:21 -0500

    Ajuste en tipados

--- src/app/services/utils.ts
+++ src/app/services/utils.ts
@@ -1,6 +1,7 @@
 import axiosInstance from "@services/axiosInstance";
+import { DocumentTypeI } from "../common/interfaces/utils";
 
-export const getDocumentTypes = async (): Promise<any> => {
+export const getDocumentTypes = async (): Promise<DocumentTypeI[]> => {
   const response = await axiosInstance.get("/utils/document-types");
   return response.data;
 };
